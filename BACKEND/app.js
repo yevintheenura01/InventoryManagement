b7eda38f1b95aa6c5205bc90433a41b19bdc87a0
Yevin Theenura <99358832+yevintheenura01@users.noreply.github.com>
2024-07-03 22:53:58 +0530

    first commit

---
+++ BACKEND/app.js
@@ -0,0 +1,36 @@
+//pswrd= fsJOGWSDQ18jlrVg
+
+const express = require("express");
+const mongoose = require("mongoose");
+const Vroute = require("./Routes/VarietyRoute");
+const Proute = require("./Routes/PaddyRoute");
+const Rroute = require("./Routes/RiceRoute");
+const LocRoute = require("./Routes/LocationRoute");
+const StoringRoute = require("./Routes/StoringRoute");
+const PaddyWarehouse = require("./Routes/PaddyWarehouseRoute");
+const InstructionRoute = require("./Routes/InstructionRoute");
+
+const app = express();
+const cors = require("cors");
+//app.use(bodyParser.json()); // Parse JSON bodies
+
+//middleware
+app.use(express.json());
+app.use(cors());
+app.use("/varieties", Vroute);
+app.use("/paddy", Proute);
+app.use("/rice", Rroute);
+app.use("/location", LocRoute);
+app.use("/storingInst", StoringRoute);
+app.use("/relationship", PaddyWarehouse);
+app.use("/api",InstructionRoute);
+
+mongoose
+  .connect(
+    "mongodb+srv://yevin:fsJOGWSDQ18jlrVg@cluster0.6bn7jxc.mongodb.net/"
+  )
+  .then(() => console.log("Connected to MongoDB"))
+  .then(() => {
+    app.listen(5000);
+  })
+  .catch((err) => console.log);
