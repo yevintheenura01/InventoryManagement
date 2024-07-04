bbde1c271bff55ec470a8a941266d996e32c943f
Yevin Theenura <99358832+yevintheenura01@users.noreply.github.com>
2024-07-04 19:28:57 +0530

    main commit

--- frontend/src/Components/main/Main.js
+++ frontend/src/Components/main/Main.js
@@ -1,18 +1,33 @@
-import React from 'react';
+import React from "react";
 import { Link } from "react-router-dom";
-import { AppBar, Toolbar, Typography, Button, Grid, Paper } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-import { Assignment, Storage, Build, People, PersonOutline, MonetizationOn } from '@material-ui/icons';
+import {
+  Button,
+  Grid,
+  Paper,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import Person2Icon from '@mui/icons-material/Person2';
+import EngineeringIcon from '@mui/icons-material/Engineering';
 
 import Head from "./MainHeader";
+import backgroundImage from './green-field.jpg'; // Update the path to your image
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
     padding: theme.spacing(2),
+    backgroundImage: `url(${backgroundImage})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    height: '100vh',
+    width: '100%',
   },
-  title: {
-    flexGrow: 1,
+  paper: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100%",
+    backgroundColor: "rgba(255, 255, 255, 0.5)", // Adjust the transparency here (0.5 is 50% opacity)
   },
   button: {
     margin: theme.spacing(2),
@@ -25,65 +40,32 @@ function Main() {
 
   return (
     <div className={classes.root}>
-      <AppBar position="static" >
-        <Toolbar>
-          <Typography variant="h6" className={classes.title}>
-            Admin Dashboard
-          </Typography>
-        </Toolbar>
-      </AppBar>
       <Head />
-      <Grid container spacing={3} justify="center" style={{marginTop:"2em"}}>
-        <Grid item xs={12} sm={6} md={4}>
-          <Paper elevation={3}>
-            <Link to="/tharaka" style={{ textDecoration: 'none' }}>
-              <Button variant="contained" color="primary" className={classes.button} startIcon={<Assignment />}>
-                Sales and Invoice
+      <Grid container spacing={3} justify="center" style={{ marginTop: "2em" }}>
+        <Grid item xs={2} sm={2} md={4}>
+          <Paper elevation={5} className={classes.paper}>
+            <Link to="/yevin" style={{ textDecoration: "none" }}>
+              <Button
+                variant="contained"
+                color="primary"
+                className={classes.button}
+                startIcon={<Person2Icon />}
+              >
+                Admin
               </Button>
             </Link>
           </Paper>
         </Grid>
-        <Grid item xs={12} sm={6} md={4}>
-          <Paper elevation={3}>
-            <Link to="/yevin" style={{ textDecoration: 'none' }}>
-              <Button variant="contained" color="primary" className={classes.button} startIcon={<Storage />}>
-                Inventory Management
-              </Button>
-            </Link>
-          </Paper>
-        </Grid>
-        <Grid item xs={12} sm={6} md={4}>
-          <Paper elevation={3}>
-            <Link to="/manoj" style={{ textDecoration: 'none' }}>
-              <Button variant="contained" color="primary" className={classes.button} startIcon={<Build />}>
-                Production Management
-              </Button>
-            </Link>
-          </Paper>
-        </Grid>
-        <Grid item xs={12} sm={6} md={4}>
-          <Paper elevation={3}>
-            <Link to="/nalinda" style={{ textDecoration: 'none' }}>
-              <Button variant="contained" color="primary" className={classes.button} startIcon={<People />}>
-                HR Management
-              </Button>
-            </Link>
-          </Paper>
-        </Grid>
-        <Grid item xs={12} sm={6} md={4}>
-          <Paper elevation={3}>
-            <Link to="/chathumin" style={{ textDecoration: 'none' }}>
-              <Button variant="contained" color="primary" className={classes.button} startIcon={<PersonOutline />}>
-                CR Management
-              </Button>
-            </Link>
-          </Paper>
-        </Grid>
-        <Grid item xs={12} sm={6} md={4}>
-          <Paper elevation={3}>
-            <Link to="/senuri" style={{ textDecoration: 'none' }}>
-              <Button variant="contained" color="primary" className={classes.button} startIcon={<MonetizationOn />}>
-                Finance
+        <Grid item xs={2} sm={2} md={4}>
+          <Paper elevation={3} className={classes.paper}>
+            <Link to="/wHome" style={{ textDecoration: "none" }}>
+              <Button
+                variant="contained"
+                color="primary"
+                className={classes.button}
+                startIcon={<EngineeringIcon />}
+              >
+                Workers
               </Button>
             </Link>
           </Paper>
