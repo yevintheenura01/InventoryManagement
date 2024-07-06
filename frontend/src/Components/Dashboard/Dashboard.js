428c2f9a0e1a38e12cc8e1fd6c47012dce514570
Yevin Theenura <99358832+yevintheenura01@users.noreply.github.com>
2024-07-06 14:25:15 +0530

    del

--- frontend/src/Components/Dashboard/Dashboard.js
+++ frontend/src/Components/Dashboard/Dashboard.js
@@ -150,8 +150,6 @@ import { BarChart, Bar, XAxis, YAxis, Tooltip, Legend, ResponsiveContainer } fro
 import Header from '../Header/Header';
 import RefreshIcon from '@mui/icons-material/Refresh';
 
-import config from "../config";
-
 function Dashboard() {
   const [totalCount, setTotalCount] = useState(0);
   const [totalPrice, setTotalPrice] = useState(0);
@@ -163,19 +161,19 @@ function Dashboard() {
     try {
       setLoading(true);
       // Fetch total count
-      const countResponse = await axios.get(`${config.backendUrl}/varieties`);
+      const countResponse = await axios.get('http://localhost:5000/varieties');
       setTotalCount(countResponse.data.totalCount);
 
       // Fetch total price
-      const priceResponse = await axios.get(`${config.backendUrl}/paddy/totalPrice`);
+      const priceResponse = await axios.get('http://localhost:5000/paddy/totalPrice');
       setTotalPrice(priceResponse.data.totalPrice);
 
       // Fetch total weight
-      const weightResponse = await axios.get(`${config.backendUrl}/paddy/totalWeight`);
+      const weightResponse = await axios.get('http://localhost:5000/paddy/totalWeight');
       setTotalWeight(weightResponse.data.totalWeight);
 
       // Fetch paddy data for the chart
-      const paddyResponse = await axios.get(`${config.backendUrl}/paddy`);
+      const paddyResponse = await axios.get('http://localhost:5000/paddy');
       setPaddyData(transformData(paddyResponse.data.paddy));
     } catch (error) {
       console.error('Error fetching data:', error);
@@ -186,7 +184,7 @@ function Dashboard() {
 
   useEffect(() => {
     fetchData();
-  });
+  }, []);
 
   // Function to transform paddy data into chart-compatible format
   const transformData = (paddyData) => {
--- frontend/src/Components/config.js
+++
@@ -1,7 +0,0 @@
-// src/config.js
-const config = {
-    backendUrl: "https://inventory-management-backend-three.vercel.app",
-  };
-  
-  export default config;
-  
